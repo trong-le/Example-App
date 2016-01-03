525c18225ce0a120d6c3ce4f07a3eb0fd6108bf0
trong-le <trong.p.le.92@gmail.com>
2016-01-03 16:56:52 -0500

    Add comments to json controller

--- example/JSONDataTableViewController.swift
+++ example/JSONDataTableViewController.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
+// Cell identifier and JSOn data path
 private let reuseIdentifier = "cell"
 private let urlPath = "http://jsonplaceholder.typicode.com/posts"
 
 class JSONDataTableViewController: UITableViewController, NSURLSessionDataDelegate {
 
+    // Get URL request and declare session and array
     var request = NSURLRequest(URL: NSURL(string: urlPath)!)
     var session: NSURLSession!
     var arr = [[String : AnyObject]]()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set up session to transfer data
         session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: self, delegateQueue: nil)
         let task = session.dataTaskWithRequest(request) { [weak self] (data, response, error) -> Void in
+            
+            // Make sure data isn't nil
             if let data = data {
                 do {
+                    
+                    // Convert objects to correct types and load in table and array
                     let json =  try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as! [[String: AnyObject]]
                     self?.arr = json
                     self?.tableView.reloadData()
@@ -34,14 +43,17 @@ class JSONDataTableViewController: UITableViewController, NSURLSessionDataDelega
     
     // MARK: - Table view data source
 
+    // Only 1 section being reused numerous times
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
+    // Number of rows there will be from given array and data
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arr.count
     }
     
+    // Fill in sections and rows with data from url path
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
         let dat = arr[indexPath.row]
