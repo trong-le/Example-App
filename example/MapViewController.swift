f347f5967a6bf96e8f7f330f4d029e3082ca6ccc
trong-le <trong.p.le.92@gmail.com>
2016-01-03 16:46:46 -0500

    Add annotations for rest of cities

--- example/MapViewController.swift
+++ example/MapViewController.swift
@@ -11,6 +11,7 @@ import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
 
+    // Set map view delegate and view type
     @IBOutlet weak var mapView: MKMapView! {
         didSet {
             mapView.mapType = .Satellite
@@ -23,14 +24,35 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
         // City locations
         let seattleLocation = CLLocationCoordinate2D(latitude: 47.6097, longitude: -122.3331)
+        let hillsboroLocation = CLLocationCoordinate2D(latitude: 39.2058, longitude: -83.6139)
+        let columbusLocation = CLLocationCoordinate2D(latitude: 39.9833, longitude: -82.9833)
+        let cincinnatiLocation = CLLocationCoordinate2D(latitude: 39.1000, longitude: -84.5167)
         
         // Add annotations
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = seattleLocation
-        annotation.title = "Seattle, Washington"
-        annotation.subtitle = "My hometown"
-        mapView.addAnnotation(annotation)
-
+        let annotationSeattle = MKPointAnnotation()
+        annotationSeattle.coordinate = seattleLocation
+        annotationSeattle.title = "Seattle, Washington"
+        annotationSeattle.subtitle = "Where I was born."
+        mapView.addAnnotation(annotationSeattle)
+        
+        let annotationHillsboro = MKPointAnnotation()
+        annotationHillsboro.coordinate = hillsboroLocation
+        annotationHillsboro.title = "Hillsboro, Ohio"
+        annotationHillsboro.subtitle = "Where I went to high school."
+        mapView.addAnnotation(annotationHillsboro)
+        
+        let annotationColumbus = MKPointAnnotation()
+        annotationColumbus.coordinate = columbusLocation
+        annotationColumbus.title = "Columbus, Ohio"
+        annotationColumbus.subtitle = "Where I attended college. Go Bucks!"
+        mapView.addAnnotation(annotationColumbus)
+        
+        let annotationCincinnati = MKPointAnnotation()
+        annotationCincinnati.coordinate = cincinnatiLocation
+        annotationCincinnati.title = "Cincinnati, Ohio"
+        annotationCincinnati.subtitle = "Where I currently reside."
+        mapView.addAnnotation(annotationCincinnati)
+        
     }
 
     
