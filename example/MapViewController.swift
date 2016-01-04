b209a5dcf22e3b5023757bcad30ba323c73cf89e
trong-le <trong.p.le.92@gmail.com>
2016-01-03 23:02:56 -0500

    Add segues from map accessoryviews to image view controller

--- example/MapViewController.swift
+++ example/MapViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 import MapKit
 
+private let identifier = "Seattle"
+
 class MapViewController: UIViewController, MKMapViewDelegate {
 
     // Set map view delegate and view type
     @IBOutlet weak var mapView: MKMapView! {
         didSet {
-            mapView.mapType = .Satellite
+            //mapView.mapType = .Satellite
             mapView.delegate = self
         }
     }
+    
+    private var seattleDescription: String = ""
+    private var hillsboroDescription: String = ""
+    private var columbusDescription: String = ""
+    private var cincinnatiDescription: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,24 +40,28 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         annotationSeattle.coordinate = seattleLocation
         annotationSeattle.title = "Seattle, Washington"
         annotationSeattle.subtitle = "Where I was born."
+        seattleDescription = annotationSeattle.description
         mapView.addAnnotation(annotationSeattle)
         
         let annotationHillsboro = MKPointAnnotation()
         annotationHillsboro.coordinate = hillsboroLocation
         annotationHillsboro.title = "Hillsboro, Ohio"
         annotationHillsboro.subtitle = "Where I went to high school."
+        hillsboroDescription = annotationHillsboro.description
         mapView.addAnnotation(annotationHillsboro)
         
         let annotationColumbus = MKPointAnnotation()
         annotationColumbus.coordinate = columbusLocation
         annotationColumbus.title = "Columbus, Ohio"
         annotationColumbus.subtitle = "Where I attended college. Go Bucks!"
+        columbusDescription = annotationColumbus.description
         mapView.addAnnotation(annotationColumbus)
         
         let annotationCincinnati = MKPointAnnotation()
         annotationCincinnati.coordinate = cincinnatiLocation
         annotationCincinnati.title = "Cincinnati, Ohio"
         annotationCincinnati.subtitle = "Where I currently reside."
+        cincinnatiDescription = annotationCincinnati.description
         mapView.addAnnotation(annotationCincinnati)
         
     }
@@ -58,7 +69,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     // Set up callout accessory view (Right info button)
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
         
-        var view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "Seattle")
+        let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
         view.canShowCallout = true
         let but = UIButton(type: .DetailDisclosure)
         view.rightCalloutAccessoryView = but
@@ -66,6 +77,50 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         return view
     }
     
+    // What the info button does
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == view.rightCalloutAccessoryView {
+            if view.annotation?.description == seattleDescription {
+                return performSegueWithIdentifier("Seattle", sender: self)
+            }
+            
+            if view.annotation?.description == hillsboroDescription {
+                return performSegueWithIdentifier("Hillsboro", sender: self)
+            }
+            
+            if view.annotation?.description == columbusDescription {
+                return performSegueWithIdentifier("Columbus", sender: self)
+            }
+            
+            if view.annotation?.description == cincinnatiDescription {
+                return performSegueWithIdentifier("Cincinnati", sender: self)
+            }
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController {
+            if let identifier = segue.identifier {
+                switch identifier {
+                case "Seattle":
+                    ivc.imageURL = Locations.Seattle
+                    ivc.title = "Seattle, Washington"
+                case "Hillsboro":
+                    ivc.imageURL = Locations.Hillsboro
+                    ivc.title = "Hillsboro, Ohio"
+                case "Columbus":
+                    ivc.imageURL = Locations.Columbus
+                    ivc.title = "Columbus, Ohio"
+                case "Cincinnati":
+                    ivc.imageURL = Locations.Cincinnati
+                    ivc.title = "Cincinnati, Ohio"
+                default: break
+                }
+            }
+        }
+    }
+
 
     /*
     // MARK: - Navigation
