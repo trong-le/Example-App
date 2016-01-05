0efa66ac1ed6c1778c10bde7209c50032aa5b5d4
trong-le <trong.p.le.92@gmail.com>
2016-01-04 22:15:05 -0500

    Set up login storyboard layout

--- example/LoginViewController.swift
+++ example/LoginViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
